1d308762d89bf1252466eb623d3e62cbda006068
Yogesh Jangid <yogesh@localhost.localdomain>
2022-03-26 11:03:51 +0530

    Lecture 1 is completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className='flex justify-between items-center bg-purple-500 text-white p-4 px-8'>
+        <h2 className='uppercase font-bold text-xl'>TextUtils</h2>
+        <ul className='flex gap-8'>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Shop</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Contact</a></li>
+        </ul>
       </header>
-    </div>
+    </>
   );
 }
 
